e304bf738af71e85573a7cf64d0572ed3f56624b
momoclouq <phamhoangminh050899@gmail.com>
2021-09-16 09:44:32 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res, next) => {
+    return res.json({
+        hello: "hello world"
+    })
+})
+
+module.exports = app;
